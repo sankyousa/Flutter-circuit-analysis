e8773f87d9fa9a8d4d99d727931c2687f52827be
sankyousa <f535026245@gmail.com>
2026-03-23 21:13:56 +0800

    Fix Buck converter preset, add Fig 6.5 circuit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3059447141"
+    serviceWorkerVersion: "3035624262"
   }
 });
